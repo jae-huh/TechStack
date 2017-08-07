cb25eb13bff650af223aca316e1e58ee7891946b
Jae Huh <jaeeunhuh@gmail.com>
2017-08-07 17:49:38 +1000

    Use redux to access the store for library data - 12.90

---
+++ src/components/LibraryList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+class LibraryList extends React.Component {
+  render() {
+    console.log(this.props)
+    return
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    libraries: state.libraries
+  }
+}
+
+export default connect(mapStateToProps)(LibraryList)
